5d5e1286d0462b526cdee85dd190846c02fd3c4d
Git-of-Ritesh <rajpandey3942@gmail.com>
2025-03-19 10:53:50 +0530

    about section commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Navbar from "@/app/components/Navbar/navbar";
 import Home_model from "@/app/components/3D_models/home_model";
 import Circular_text from '@/app/components/circular_text/circularText'
 import ResumeButton from "./components/Buttons/resumeButton";
+import FourModels from "./components/3D_models/fourModels";
 
 export default function Home() {
 
@@ -46,11 +47,49 @@ export default function Home() {
         <Circular_text />
       </div>
 
+      {/* imaginary div to prevent overlapping */}
       <div className="h-dvh w-screen bottom-4">
-
       </div>
 
-      <div className="h-200 z-50 bg-gray-100">
+      <div className="z-50 w-full flex-col justify-center items-center bg-gray-100 space-y-2 sm:space-y-20">
+
+        <div className="sm:hidden px-7 py-40 space-y-4">
+          <h2 className="text-base font-medium text-black">I’m Ritesh — a software developer and UI/UX designer. I build user-friendly apps by blending clean code with creative design.</h2>
+          <h2 className="text-base font-medium text-black ">Specializing in full-stack development, I turn ideas into smooth, high-performing digital products.</h2>
+          <h2 className="text-base font-medium text-black">Passionate about technology and creativity, I constantly explore new tools and design trends to craft better apps.</h2>
+          <h2 className="text-base font-medium text-black">Based in India, I’m open to work and collaborations from 10 AM to 8 PM (IST).</h2>
+        </div>
+
+        <div className="hidden relative w-full sm:flex justify-center items-center">
+          {/* 3D Model Component */}
+          <FourModels />
+
+          {/* Transparent Glassmorphic Card Overlay */}
+          <div className="absolute top-[55%] -translate-y-1/2 w-80 sm:w-200 p-6 rounded-2xl bg-white/10 backdrop-blur-lg shadow-lg border space-y-5 border-white/20">
+            <h2 className="text-xl font-medium text-black">I’m Ritesh — a software developer and UI/UX designer. I build user-friendly apps by blending clean code with creative design.</h2>
+            <h2 className="text-xl font-medium text-black ">Specializing in full-stack development, I turn ideas into smooth, high-performing digital products.</h2>
+            <h2 className="text-xl font-medium text-black">Passionate about technology and creativity, I constantly explore new tools and design trends to craft better apps.</h2>
+            <h2 className="text-xl font-medium text-black">Based in India, I’m open to work and collaborations from 10 AM to 8 PM (IST).</h2>
+          </div>
+
+          <div className="absolute bottom-0 right-0 bg-gray-100 w-40 h-20"></div>
+        </div>
+
+        {/* selected word section */}
+        <div className="p-12">
+          <h1 className="text-black text-xl sm:text-4xl font-medium">SELECTED WORKS</h1>
+          <hr className="text-black mt-5" />
+
+          {/* project cards */}
+          <div>
+            {/* project image */}
+          <div></div>
+            {/* project discription */}
+            <div></div>
+          </div>
+        </div>
+
+
 
       </div>
 
